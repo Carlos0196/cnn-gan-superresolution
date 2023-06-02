5a3dcc595c9efe27bec5c1875e26c4f8e9bfed2a
Carlos J. Guzman Guell <cguzmanguell@gmail.com>
2023-06-02 21:02:54 +0200

    Added models

---
+++ models/discriminator.py
@@ -0,0 +1,26 @@
+from constants import *
+
+import tensorflow as tf
+from tensorflow.keras import layers
+
+def make_discriminator_model():
+  model = tf.keras.Sequential()
+
+  model.add(layers.Input((HR_IMG_SIZE, HR_IMG_SIZE, 3)))
+
+  model.add(layers.Conv2D(128, (3,3), strides=(1,1)))
+  model.add(layers.LeakyReLU())
+  model.add(layers.Dropout(0.3))
+
+  model.add(layers.Conv2D(64, (5,5), strides=(2,2)))
+  model.add(layers.LeakyReLU())
+  model.add(layers.Dropout(0.3))
+
+  model.add(layers.Conv2D(32, (7,7), strides=(2,2)))
+  model.add(layers.LeakyReLU())
+  model.add(layers.Dropout(0.3))
+
+  model.add(layers.Flatten())
+  model.add(layers.Dense(1))
+
+  return model
---
+++ models/generator.py
@@ -0,0 +1,25 @@
+from constants import *
+
+import tensorflow as tf
+from tensorflow.keras import layers
+
+def make_generator_model():
+  model = tf.keras.Sequential()
+
+  model.add(layers.Input((LR_IMG_SIZE, LR_IMG_SIZE, 3)))
+
+  model.add(layers.Conv2DTranspose(256, (7,7), strides=(1,1), padding='same'))
+  model.add(layers.BatchNormalization())
+  model.add(layers.LeakyReLU())
+
+  model.add(layers.Conv2DTranspose(128, (5,5), strides=(1,1), padding='same'))
+  model.add(layers.BatchNormalization())
+  model.add(layers.LeakyReLU())
+
+  model.add(layers.Conv2DTranspose(64, (3,3), strides=(3,3)))
+  model.add(layers.BatchNormalization())
+  model.add(layers.LeakyReLU())
+
+  model.add(layers.Conv2DTranspose(3, (3,3), strides=(1,1), padding='same'))
+
+  return model
