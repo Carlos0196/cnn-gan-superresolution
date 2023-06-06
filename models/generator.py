365d243030aab23222165c5cddaeda768d0fc618
Carlos J. Guzman Guell <cguzmanguell@gmail.com>
2023-06-06 11:52:58 +0200

    OOM fix

--- models/generator.py
+++ models/generator.py
@@ -8,16 +8,16 @@ def make_generator_model():
     model.add(layers.Input((None, None, 3)))
 
     model.add(
-        layers.Conv2DTranspose(256, (3, 3), strides=(1, 1), padding='same'))
+        layers.Conv2DTranspose(128, (3, 3), strides=(1, 1), padding='same'))
     model.add(layers.BatchNormalization())
     model.add(layers.LeakyReLU())
 
     model.add(
-        layers.Conv2DTranspose(512, (3, 3), strides=(1, 1), padding='same'))
+        layers.Conv2DTranspose(256, (3, 3), strides=(1, 1), padding='same'))
     model.add(layers.BatchNormalization())
     model.add(layers.LeakyReLU())
 
-    model.add(layers.Conv2DTranspose(1024, (3, 3), strides=(3, 3)))
+    model.add(layers.Conv2DTranspose(512, (3, 3), strides=(3, 3)))
     model.add(layers.BatchNormalization())
     model.add(layers.LeakyReLU())
 
