b7a2d9a3375ede83066a434ea36ea7559fc815d8
Carlos J. Guzman Guell <cguzmanguell@gmail.com>
2023-06-12 08:50:13 +0200

    Sigmoid

--- models/discriminator.py
+++ models/discriminator.py
@@ -18,6 +18,6 @@ def make_discriminator_model():
     model.add(layers.Dropout(0.3))
 
     model.add(layers.Flatten())
-    model.add(layers.Dense(1))
+    model.add(layers.Dense(1, activation='sigmoid'))
 
     return model
